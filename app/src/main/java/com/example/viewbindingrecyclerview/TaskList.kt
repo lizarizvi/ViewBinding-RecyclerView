93bd72da858a1554ad3805b410a827e3873105b6
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-15 15:23:50 +0530

    Creating a list of tasks

---
+++ app/src/main/java/com/example/viewbindingrecyclerview/TaskList.kt
@@ -0,0 +1,20 @@
+package com.example.viewbindingrecyclerview
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("TAKE A WALK", timeStamp = "8:00 am"),
+        Task("EAT BREAKFAST", timeStamp = "9:00 am"),
+        Task("WATCH YOUTUBE", timeStamp = "10:00 am"),
+        Task("TAKE A BATH", timeStamp = "10:30 am"),
+        Task("STUDY TIME", timeStamp = "11:30 am"),
+        Task("EAT LUNCH", timeStamp = "1:30 pm"),
+        Task("STUDY TIME", timeStamp = "2:00 pm"),
+        Task("WATCH K-DRAMA", timeStamp = "4:00 pm"),
+        Task("STUDY TIME", timeStamp = "5:00 pm"),
+        Task("EAT SNACK", timeStamp = "6:30 pm"),
+        Task("STUDY TIME", timeStamp = "7:00 pm"),
+        Task("EAT DINNER", timeStamp = "9:30 pm"),
+        Task("WATCH K-DRAMA", timeStamp = "10:00 pm"),
+        Task("GO TO BED", timeStamp = "11:00 pm")
+    )
+}
